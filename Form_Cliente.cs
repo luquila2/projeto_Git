44e917fde0ec87102489368c24d3685810f80b90
luquila2 <lucas.aquila.silva@hotmail.com>
2024-02-08 16:53:12 -0300

    Adilçao de novo repositório

---
+++ Form_Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_Git
+{
+    public partial class Form_Cliente : Form
+    {
+        public Form_Cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
